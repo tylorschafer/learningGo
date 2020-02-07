eaf1c443341ce5bde58c37a4a7e4f398d56e2b71
Tylor Schafer <tylorschafer@gmail.com>
2020-02-07 14:41:21 -0700

    Add note on function visability

--- src/shopping/db/db.go
+++ src/shopping/db/db.go
@@ -4,6 +4,7 @@ type Item struct {
   Price float64
 }
 
+// functions starting with Upper Case Letter are visable on imports. Lower case functions are not
 func LoadItem(id int) *Item {
   return &Item{
     Price: 9.001,
